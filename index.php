52be8c6a678b1bc9f450e92712987aea0ceeff59
dandoridwanto <dandoridwanto@ymail.com>
2020-08-03 15:22:40 +0700

    merubah file index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum similique a totam pariatur autem, quasi saepe sint, sequi obcaecati at dicta debitis fuga architecto id iusto? Modi rem dolores totam.
 </body>
 </html>
